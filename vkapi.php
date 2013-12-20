9f43b07ba6e826b9c5cc8dfb0627c8e493e967da
PavelK <2me.pavelk@gmail.com>
2013-12-21 01:04:22 +0400

    132

--- vkapi.php
+++ vkapi.php
@@ -55,6 +55,8 @@ class Vkapi {
 	
 	public static function refresh() {
 		if (isset($_POST['captcha_sid'])) {
+			echo $_POST['_wp_http_referer'].'&captcha_sid='.$_POST['captcha_sid'].'&captcha_key='.$_POST['captcha_key'];
+			die();
 			wp_redirect($_POST['_wp_http_referer'].'&captcha_sid='.$_POST['captcha_sid'].'&captcha_key='.$_POST['captcha_key']);
 		}
 	}
