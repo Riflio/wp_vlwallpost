332c23ce7c83233efe46f50519d972ea11209053
PavelK <2me.pavelk@gmail.com>
2013-12-23 23:16:45 +0400

    Правки

--- listtablegroups.php
+++ listtablegroups.php
@@ -15,7 +15,7 @@ class VKWP__List_Table extends WP_List_Table {
 	
 	function extra_tablenav( $which ) {
 		echo '
-			<ul class="subsubsub">
+			<ul class="subsubsub"> 
 				<li class="all">
 									
 				</li>
@@ -28,7 +28,7 @@ class VKWP__List_Table extends WP_List_Table {
 			'id' 		=> __('id'),
 			'title'		=> __('Title'),
 			'album'		=> __('Album'),
-			'export'	=> '<input type="checkbox" name="cb_exportall" id="#cb_exportall" checked value="true"  />'.__('Export')
+			'export'	=> '<input type="checkbox" name="cb_exportall" id="cb_exportall" checked value="true"  />'.__('Export')
 		);
 		return $columns;
 	}	
