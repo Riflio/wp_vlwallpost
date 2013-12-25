bbfaac24bd2463ef9dd6ff610273b05794a5b395
PavelK <2me.pavelk@gmail.com>
2013-12-26 00:47:07 +0400

    Тоже

--- vkwallpost.php
+++ vkwallpost.php
@@ -144,7 +144,7 @@ class VKWallPost {
 		$_export=urldecode($_GET['export']);
 		$export  = json_decode($_export);
 		$limit=1; //TODO: Добавить в настройки сколько за раз
-		$posts=$wpdb->get_results("SELECT * FROM {$wpdb->prefix}vktemp  LIMIT {$export->step, $limit} ;");
+		$posts=$wpdb->get_results("SELECT * FROM {$wpdb->prefix}vktemp  LIMIT  {$export->step}, {$limit} ;");
 
 		$nowDT = new DateTime();
 		$nowDT=$nowDT->format('Y-m-d H:i:s');		
